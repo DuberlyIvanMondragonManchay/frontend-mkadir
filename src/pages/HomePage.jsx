b2043ddb73657ba16a4c79101b61a75e643ca074
DuberlyIvanMondragonManchay <duberly.mondragon@tecsup.edu.pe>
2023-09-24 08:32:22 -0500

    Adding a data filter to the restaurant

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,37 +5,48 @@ import CardRestaurant from '../components/CardRestaurant'
 import { getResturantsRequest } from '../api/Restaurant.pi'
 export default function HomePage() {
   const [restaurants,setRestaurants] = useState([])
-  const [search,setSearch] = useState(null)
+  const [search,setSearch] = useState('')
 
+  const showData = async()=>{
+    const res = await getResturantsRequest()
+    setRestaurants(res.data)
+  }
   const searcher = (e) => {
     setSearch(e.target.value)
-    console.log(e.target.value)
   }
+  // FILTER DATA
+  const results = !search ? restaurants : restaurants.filter((dato)=>dato.name.toLowerCase().includes(search.toLowerCase()))
+
 
   useEffect(()=>{
-    async function getRestaurants() {
-      const res = await getResturantsRequest()
-      setRestaurants(res.data)
-    }
-    if(restaurants.length <=0){
-      getRestaurants()
-    }
-  },[restaurants])
+    showData()
+  },[])
 
-  useEffect(()=>{
-    console.log(restaurants)
-  },[restaurants])
+
+
+
+  // useEffect(()=>{
+  //   async function getRestaurants() {
+  //     const res = await getResturantsRequest()
+  //     setRestaurants(res.data)
+  //   }
+  //   if(restaurants.length <=0){
+  //     getRestaurants()
+  //   }
+  // },[restaurants])
+
+
   return (
     <div className='m-2 mt-4'>
         <h1 className='text-3xl my-5 text-center'>Restaurantes</h1>
         <CarrouselComponent/>
         <SearchComponent value={search} onChange={searcher}/>
         
-        {restaurants.map((restaurants, index) =>(
+        {results.map((restaurant, index) =>(
           <CardRestaurant 
           key={index}
-          card_img={restaurants.logo_url}
-          card_title={restaurants.name}
+          card_img={restaurant.logo_url}
+          card_title={restaurant.name}
           />
         ))}
         
