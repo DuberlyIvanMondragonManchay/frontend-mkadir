af0ce26293cf025f7c2eba4cb51c15b3b3b01dc8
DuberlyIvanMondragonManchay <duberly.mondragon@tecsup.edu.pe>
2023-10-13 15:29:21 -0500

    Created profile

--- src/pages/profile/ProfilePageIndex.jsx
+++ src/pages/profile/ProfilePageIndex.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
-
+import { useAuth } from '../../context/AuthContext'
 export default function ProfilePageIndex() {
+  const { user } = useAuth()
+  console.log(user)
   return (
-    <div>ProfilePageIndex</div>
+    <div className="w-full mx-auto h-full shadow-lg rounded-lg overflow-hidden">
+      <h1 className='text-2xl my-5 text-center'>Mi perfil</h1>
+      <div className="bg-gray-300 h-40"></div> {/* Portada gris */}
+      <img src={user.picture} alt="Profile Picture" className="w-32 h-32 sm:w-32 sm:h-32 rounded-full border-4 border-white -mt-12 sm:-mt-16 mx-auto"/>
+      <div className="py-4 px-6 text-center">
+        <p className="text-lg font-semibold mb-2">{user.name} {user.paternal_surname} {user.maternal_surname} </p>
+      </div>
+    </div>
+
   )
 }
