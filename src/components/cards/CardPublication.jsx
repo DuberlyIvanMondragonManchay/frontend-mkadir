9d6a6cc27386a2d016ea1ea73138282ec8f28220
DuberlyIvanMondragonManchay <duberly.mondragon@tecsup.edu.pe>
2023-10-27 01:17:31 -0500

    Edit CardPublication

--- src/components/cards/CardPublication.jsx
+++ src/components/cards/CardPublication.jsx
@@ -1,10 +1,9 @@
-
 import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-const CardPublication = (props) => {
 
+const CardPublication = (props) => {
   const settings = {
     infinite: true,
     speed: 500,
@@ -14,11 +13,15 @@ const CardPublication = (props) => {
 
   return (
     <div className="carousel-container">
-      <Slider {...settings} className='border m-0 p-0'>
+      <Slider {...settings}>
         {props.carouselData.map((item, index) => (
-          <div key={index}>
-            <p className='ml-2'>{item.description}</p>
-            <img src={item.img_menu_url} alt={`Imagen ${props.menu_name}`} />
+          <div key={index} style={{ height: "300px" }} className="p-4 overflow-hidden flex flex-col">
+            <div style={{ height:"50px" }} className="mb-1 overflow-y-auto">
+              <p className='ml-2'>{item.description}</p>
+            </div>
+            <div style={{ height: "600px" }} className="max-h-full max-w-full overflow-y-auto">
+              <img className='m-auto max-h-full max-w-full object-center object-cover' src={item.img_menu_url} alt={`Imagen ${props.menu_name}`} />
+            </div>
           </div>
         ))}
       </Slider>
